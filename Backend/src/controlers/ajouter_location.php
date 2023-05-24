fe6ea2c7131b55e19dbd82efffef2ba9258bda51
UnBretonFou <119815602+UnBretonFou@users.noreply.github.com>
2023-05-24 16:44:12 +0200

    vérif disponibilité des voitures

--- Backend/src/controlers/ajouter_location.php
+++ Backend/src/controlers/ajouter_location.php
@@ -12,7 +12,28 @@
     $date_fin = $_POST["date_fin"];
     $choix_option = $_POST["choix_option"];
 
+    //Vérifier la disponnibilité de la voiture
+    $sql_disponibilite = "SELECT COUNT(*) AS count
+                          FROM location
+                          WHERE id_voiture = '$voiture'
+                          AND (date_debut <= '$date_debut' AND date_fin >= '$date_debut')
+                          OR (date_debut <= '$date_fin' AND date_fin >= '$date_fin')
+                          OR ('$date_debut' <= date_debut AND '$date_fin' >= date_debut)";
+    $result_disponibilite = mysqli_query($con, $sql_disponibilite);
+    $row_disponibilite = mysqli_fetch_array($result_disponibilite);
+    $count_disponibilite = $row_disponibilite["count"];
 
+    if ($count_disponibilite > 0) {
+        // La voiture n'est pas disponible pendant la période spécifiée
+        // Afficher un message d'erreur ou prendre une action appropriée
+        // Par exemple :
+        echo "La voiture n'est pas disponible pendant la période spécifiée.";
+        // Vous pouvez rediriger vers une autre page ou afficher un message d'erreur dans le formulaire
+        // Assurez-vous d'adapter cette action en fonction de vos besoins.
+        exit();
+    }
+
+     // La voiture est disponible, procéder à l'insertion
     //Insérer la nouvelle location dans la base de données
     $sql = "INSERT INTO location (id_client, id_voiture, compteur_debut, compteur_fin, date_debut, date_fin)
             VALUES ('$client', '$voiture', '$compteur_debut', '$compteur_fin', '$date_debut', '$date_fin')";
